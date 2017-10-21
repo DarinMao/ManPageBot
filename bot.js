666c9b65e87259740a12011bd995d5ff15953537
Darin Mao <maodarin@gmail.com>
2017-10-21 19:07:42 -0400

    new bot file

--- bot.js
+++ bot.js
@@ -1,27 +1,12 @@
-var Discord = require('discord.io');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.content.substring(0, 4) == '>man') {
-       message.reply('received' + message.content);
-	}
-	if (message.content === 'ping')
+
+client.on('message', message => {
+	if (message.content == 'ping')
 	{
 		message.reply('pong');
 	}
