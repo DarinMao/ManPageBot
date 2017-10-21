e57eae0998a4514ff6fb54a746e8cfa26ec50d7a
Darin Mao <maodarin@gmail.com>
2017-10-21 18:07:20 -0400

    initial commit

---
+++ bot.js
@@ -0,0 +1,42 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 4) == '>man') {
+        var args = message.substring(4).split(' ');
+        var cmd = args[1];
+		
+		bot.sendMessage({
+			to: channelID,
+			message: 'Received: ' + cmd;
+		});
+        /*switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }*/
+     }
+});
